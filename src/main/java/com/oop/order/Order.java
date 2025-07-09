5fe7b0b6f086946506452fae4470eddb9d306f93
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-07-09 15:10:24 +0200

    refactor Order for OrderItems

--- src/main/java/com/oop/order/Order.java
+++ src/main/java/com/oop/order/Order.java
@@ -1,9 +1,7 @@
 package com.oop.order;
 
-import com.oop.product.Product;
-
 import java.util.Map;
 import java.util.UUID;
 
-public record Order(UUID id, Map<UUID, Product> products) {
+public record Order(UUID id, Map<UUID, OrderItem> items) {
 }
