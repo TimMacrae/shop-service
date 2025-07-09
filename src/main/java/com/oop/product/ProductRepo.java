d6e9e6f3c081071a3684e24646af7048350eb697
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-07-09 15:11:39 +0200

    add new methods checkAvailabilityAndStockQuantity and decreaseStockQuantity

--- src/main/java/com/oop/product/ProductRepo.java
+++ src/main/java/com/oop/product/ProductRepo.java
@@ -1,5 +1,6 @@
 package com.oop.product;
 
+
 import java.util.*;
 
 public class ProductRepo {
@@ -25,4 +26,20 @@ public class ProductRepo {
         return new ArrayList<>(this.products.values());
     }
 
+
+    public void checkAvailabilityAndStockQuantity (UUID id, int quantity) {
+       Product product = getProduct(id);
+       if(product.stockQuantity() < quantity) throw new IllegalArgumentException("Product with id " + id + " out of stock");
+    }
+
+
+    public Product decreaseStockQuantity(UUID id, int quantity) {
+        Product product = getProduct(id);
+        int newStockQuantity = product.stockQuantity() - quantity;
+        if (newStockQuantity < 0) throw new IllegalArgumentException("Product with id " + id + " out of stock");
+
+        Product updatedProduct = new Product(product.id(), product.name(),product.description(), product.price(), newStockQuantity);
+        products.put(id, updatedProduct);
+        return updatedProduct;
+    }
 }
