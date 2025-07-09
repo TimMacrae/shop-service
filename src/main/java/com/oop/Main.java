c69269b112883de54312b2e6d7eb84537341fafa
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-07-09 17:10:50 +0200

    add mocks to main class

--- src/main/java/com/oop/Main.java
+++ src/main/java/com/oop/Main.java
@@ -1,9 +1,56 @@
 package com.oop;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.oop.order.Order;
+import com.oop.order.OrderItem;
+import com.oop.order.OrderRepo;
+import com.oop.product.Product;
+import com.oop.product.ProductRepo;
+import com.oop.service.ShopService;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.UUID;
+
+
 public class Main {
     public static void main(String[] args) {
+        OrderRepo orderRepo = new OrderRepo();
+        ProductRepo productRepo = new ProductRepo();
+        ShopService shopService = new ShopService(orderRepo, productRepo);
+
+
+        // Create some products
+        Product productTomato = new Product(UUID.randomUUID(), "tomato", "best tomato", new BigDecimal("1.25"), 100);
+        Product productApple = new Product(UUID.randomUUID(), "apple", "best apple", new BigDecimal("1.75"), 150);
+        Product productBanana = new Product(UUID.randomUUID(), "banana", "best banana", new BigDecimal("2.00"), 80);
+        productRepo.addProduct(productTomato);
+        productRepo.addProduct(productApple);
+        productRepo.addProduct(productBanana);
+
+        // Create some OrderItems
+        OrderItem orderItemTomato = new OrderItem(productBanana.id(), productTomato.name(), productTomato.price(), 5);
+        OrderItem orderItemApple = new OrderItem(productApple.id(), productApple.name(), productApple.price(), 2);
+        OrderItem orderItemBanana = new OrderItem(productBanana.id(), productBanana.name(), productBanana.price(), 90);
+
+        // Create some Orders
+        Order order = new Order(UUID.randomUUID(), new HashMap<>() {{
+            put(orderItemTomato.productId(), orderItemTomato);
+            put(orderItemApple.productId(), orderItemApple);
+        }});
+
+        Order orderTwo = new Order(UUID.randomUUID(), new HashMap<>() {{
+            put(orderItemBanana.productId(), orderItemBanana);
+        }});
+
+        System.out.println("ORDER: " + order.toString());
+
+
+        // Place orders
+        var placedOrder = shopService.placingOrder(order);
+        System.out.println(placedOrder);
+        var placedOrderTwo = shopService.placingOrder(orderTwo);
+        System.out.println(placedOrderTwo);
+
 
     }
 }
