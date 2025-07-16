b9178b19d1b0ea3c9f8cc41a259b8e7bfb427f60
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-07-16 12:39:50 +0200

    update ShopService and tests

--- src/main/java/com/oop/service/ShopService.java
+++ src/main/java/com/oop/service/ShopService.java
@@ -1,7 +1,9 @@
 package com.oop.service;
 
+import com.oop.exception.OrderWithTheIdNotFound;
+import com.oop.exception.ProductWithIdNotFound;
+import com.oop.exception.ProductWithTheIdIsOutOfStock;
 import com.oop.order.Order;
-import com.oop.order.OrderRepo;
 import com.oop.order.OrderRepoInterface;
 import com.oop.order.OrderStatus;
 import com.oop.product.Product;
@@ -11,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-public class ShopService implements OrderRepoInterface {
+public class ShopService {
     private final OrderRepoInterface orderRepo;
     private final ProductRepo productRepo;
 
@@ -26,50 +28,70 @@ public class ShopService implements OrderRepoInterface {
             var productId = item.getKey();
             var orderItem = item.getValue();
             // This will throw if not available or not enough in stock
-            productRepo.checkAvailabilityAndStockQuantity(productId, orderItem.quantity());
+           checkAvailabilityAndStockQuantity(productId, orderItem.quantity());
         }
 
         // All products available now decrease stock and place order
         for (var item : order.items().entrySet()) {
             var productId = item.getKey();
             var orderItem = item.getValue();
-            productRepo.decreaseStockQuantity(productId, orderItem.quantity());
+            decreaseStockQuantity(productId, orderItem.quantity());
         }
 
         orderRepo.addOrder(order);
         return "Order placed successfully, the total sum is: " + order.totalSum() + " 💰";
     }
 
+    // OrderRepo
     public void addOrder(Order order) {
         orderRepo.addOrder(order);
     }
 
+    public void removeOrder(Order order) {
+        getOrder(order.id());
+        orderRepo.removeOrder(order);
+    }
+
     public Order getOrder(UUID orderId) {
-        return orderRepo.getOrder(orderId);
+        return orderRepo.getOrder(orderId).orElseThrow(()->new OrderWithTheIdNotFound(orderId));
     }
 
     public List<Order> getOrders() {
         return orderRepo.getOrders();
     }
 
-    public void removeOrder(UUID orderId) {
-        orderRepo.removeOrder(orderId);
+
+    // ProductRepo
+    public void addProduct(Product product) {
+        productRepo.addProduct(product);
     }
 
     public Product getProduct(UUID productId) {
-       return productRepo.getProduct(productId);
+        return productRepo.getProduct(productId).orElseThrow(() -> new ProductWithIdNotFound(productId));
     }
 
     public List<Product> getAllProducts() {
         return productRepo.getAllProducts();
     }
 
-    public void addProduct(Product product) {
-        productRepo.addProduct(product);
-    }
-
     public List<Order> getOrdersWithOrderStatus(OrderStatus orderStatus) {
         return orderRepo.getOrders().stream().filter(order -> orderStatus.equals(order.orderStatus())).collect(Collectors.toList());
+    }
+
+    public void checkAvailabilityAndStockQuantity (UUID id, int quantity) {
+        Product product = getProduct(id);
+        if(product.stockQuantity() < quantity) throw new ProductWithTheIdIsOutOfStock(id);
+    }
+
+
+    public Product decreaseStockQuantity(UUID id, int quantity) {
+        Product product = getProduct(id);
+        int newStockQuantity = product.stockQuantity() - quantity;
+        if (newStockQuantity < 0) throw new ProductWithTheIdIsOutOfStock(id);
+
+        Product updatedProduct = new Product(product.id(), product.name(),product.description(), product.price(), newStockQuantity);
+        productRepo.getProducts().put(id, updatedProduct);
+        return updatedProduct;
     }
 
 }
--- src/test/java/com/oop/service/ShopServiceTest.java
+++ src/test/java/com/oop/service/ShopServiceTest.java
@@ -1,5 +1,8 @@
 package com.oop.service;
 
+import com.oop.exception.OrderWithTheIdNotFound;
+import com.oop.exception.ProductWithIdNotFound;
+import com.oop.exception.ProductWithTheIdIsOutOfStock;
 import com.oop.order.*;
 import com.oop.product.Product;
 import com.oop.product.ProductRepo;
@@ -9,13 +12,12 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.util.*;
 
-import static javax.swing.UIManager.put;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShopServiceTest {
-    ProductRepo productRepo =  new ProductRepo();
-    OrderRepo orderRepo  = new OrderRepo();
-    OrderListRepo orderListRepo  = new OrderListRepo();
+    ProductRepo productRepo = new ProductRepo();
+    OrderRepo orderRepo = new OrderRepo();
+    OrderListRepo orderListRepo = new OrderListRepo();
     ShopService shopService;
 
     Product product;
@@ -33,38 +35,38 @@ class ShopServiceTest {
 
     @BeforeEach
     void setUp() {
-         product = new Product(UUID.randomUUID(),"milk", "high on magnesium", new BigDecimal("1.25"), 40);
-         product2 = new Product(UUID.randomUUID(),"apple", "very sweet", new BigDecimal("3"), 20);
+        shopService = new ShopService(orderRepo, productRepo);
+        product = new Product(UUID.randomUUID(), "milk", "high on magnesium", new BigDecimal("1.25"), 40);
+        product2 = new Product(UUID.randomUUID(), "apple", "very sweet", new BigDecimal("3"), 20);
 
-         orderItem = new OrderItem(product.id(),product.name(),  product.price(), 10);
-         orderItem2 = new OrderItem(product2.id(),product2.name(),  product2.price(), 5);
+        orderItem = new OrderItem(product.id(), product.name(), product.price(), 10);
+        orderItem2 = new OrderItem(product2.id(), product2.name(), product2.price(), 5);
 
-         productRepo.addProduct(product);
-         productRepo.addProduct(product2);
+        productRepo.addProduct(product);
+        productRepo.addProduct(product2);
 
-         order = new Order(UUID.randomUUID(), new HashMap<>() {{
-             put(orderItem.productId(), orderItem);
-             put(orderItem2.productId(), orderItem2);
-         }});
+        order = new Order(UUID.randomUUID(), new HashMap<>() {{
+            put(orderItem.productId(), orderItem);
+            put(orderItem2.productId(), orderItem2);
+        }});
 
         orderWithStatusProgress = new Order(UUID.randomUUID(), new HashMap<>() {{
             put(orderItem.productId(), orderItem);
         }});
 
         orderWithStatusInDelivery = new Order(UUID.randomUUID(), new HashMap<>() {{
-             put(orderItem.productId(), orderItem);
-             put(orderItem2.productId(), orderItem2);
-         }},null, OrderStatus.IN_DELIVERY);
+            put(orderItem.productId(), orderItem);
+            put(orderItem2.productId(), orderItem2);
+        }}, null, OrderStatus.IN_DELIVERY);
 
         orderWithStatusCompleted = new Order(UUID.randomUUID(), new HashMap<>() {{
             put(orderItem.productId(), orderItem);
-        }},null, OrderStatus.COMPLETED);
+        }}, null, OrderStatus.COMPLETED);
 
     }
 
     @Test
     void placingOrder_success() {
-        shopService = new ShopService(orderRepo, productRepo);
         Order order = new Order(UUID.randomUUID(), new HashMap<>() {{
             put(orderItem.productId(), orderItem);
             put(orderItem2.productId(), orderItem2);
@@ -88,7 +90,6 @@ class ShopServiceTest {
 
     @Test
     void placingOrder_shouldThrowAnException_productNotFound() {
-        shopService = new ShopService(orderRepo, productRepo);
         UUID id = UUID.randomUUID();
         OrderItem orderItemNotAvailable = new OrderItem(id, "mango", new BigDecimal("3"), 2);
         Order order = new Order(UUID.randomUUID(), new HashMap<>() {{
@@ -96,8 +97,8 @@ class ShopServiceTest {
             put(orderItemNotAvailable.productId(), orderItemNotAvailable);
         }});
 
-        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> shopService.placingOrder(order));
-        assertEquals("Product with id " + id + " does not exist", exception.getMessage());
+        ProductWithIdNotFound exception = assertThrows(ProductWithIdNotFound.class, () -> shopService.placingOrder(order));
+        assertEquals("Product with id " + id + " not found", exception.getMessage());
     }
 
     @Test
@@ -110,40 +111,37 @@ class ShopServiceTest {
             put(orderItemNotAvailable.productId(), orderItemNotAvailable);
         }});
 
-        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> shopService.placingOrder(order));
-        assertEquals("Product with id " + id + " does not exist", exception.getMessage());
+        ProductWithIdNotFound exception = assertThrows(ProductWithIdNotFound.class, () -> shopService.placingOrder(order));
+        assertEquals("Product with id " + id + " not found", exception.getMessage());
     }
 
     @Test
     void placingOrder_shouldThrowAnException_ifStockQuantityIsTooLow() {
-        shopService = new ShopService(orderRepo, productRepo);
-        OrderItem orderItemTooHighQuantity = new OrderItem(product2.id(),product2.name(),  product2.price(), 100);
+        OrderItem orderItemTooHighQuantity = new OrderItem(product2.id(), product2.name(), product2.price(), 100);
         Order order = new Order(UUID.randomUUID(), new HashMap<>() {{
             put(orderItem.productId(), orderItem);
             put(orderItemTooHighQuantity.productId(), orderItemTooHighQuantity);
         }});
 
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> shopService.placingOrder(order));
+        ProductWithTheIdIsOutOfStock exception = assertThrows(ProductWithTheIdIsOutOfStock.class, () -> shopService.placingOrder(order));
         assertEquals("Product with id " + orderItemTooHighQuantity.productId() + " out of stock", exception.getMessage());
     }
 
     @Test
     void placingOrder_shouldThrowAnException_ifStockQuantityIsTooLow_orderListRepo() {
-        shopService = new ShopService(orderListRepo, productRepo);
-        OrderItem orderItemTooHighQuantity = new OrderItem(product2.id(),product2.name(),  product2.price(), 100);
+        OrderItem orderItemTooHighQuantity = new OrderItem(product2.id(), product2.name(), product2.price(), 100);
         Order order = new Order(UUID.randomUUID(), new HashMap<>() {{
             put(orderItem.productId(), orderItem);
             put(orderItemTooHighQuantity.productId(), orderItemTooHighQuantity);
         }});
 
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> shopService.placingOrder(order));
+        ProductWithTheIdIsOutOfStock exception = assertThrows(ProductWithTheIdIsOutOfStock.class, () -> shopService.placingOrder(order));
         assertEquals("Product with id " + orderItemTooHighQuantity.productId() + " out of stock", exception.getMessage());
     }
 
 
     @Test
     void addOrder_shouldAddAOrder_toOrders() {
-        shopService = new ShopService(orderRepo, productRepo);
         shopService.addOrder(order);
         Order addedOrder = shopService.getOrder(order.id());
         assertEquals(addedOrder, shopService.getOrder(order.id()));
@@ -151,31 +149,27 @@ class ShopServiceTest {
 
     @Test
     void removeOrder_shouldRemoveAOrder_fromOrders() {
-        shopService = new ShopService(orderRepo, productRepo);
         shopService.addOrder(order);
         assertEquals(1, shopService.getOrders().size());
-        shopService.removeOrder(order.id());
+        shopService.removeOrder(order);
         assertEquals(0, shopService.getOrders().size());
     }
 
     @Test
     void getOrder_shouldReturnOrder_fromOrdersRepo() {
-        shopService = new ShopService(orderRepo, productRepo);
         shopService.addOrder(order);
         assertEquals(order, shopService.getOrder(order.id()));
     }
 
     @Test
     void getOrder_shouldThrowAnException_ifOrderDoesNotExist() {
-        shopService = new ShopService(orderRepo, productRepo);
         UUID id = UUID.randomUUID();
-        NoSuchElementException exception = assertThrows(NoSuchElementException.class, () -> shopService.getOrder(id));
-        assertEquals("Order with id " + id + " does not exist", exception.getMessage());
+        OrderWithTheIdNotFound exception = assertThrows(OrderWithTheIdNotFound.class, () -> shopService.getOrder(id));
+        assertEquals("Order with id " + id + " not found", exception.getMessage());
     }
 
     @Test
     void getOrders_shouldReturnOrders_fromOrderRepo() {
-        shopService = new ShopService(orderRepo, productRepo);
         Order order2 = new Order(UUID.randomUUID(), new HashMap<>() {{
             put(orderItem.productId(), orderItem);
             put(orderItem2.productId(), orderItem2);
@@ -187,21 +181,18 @@ class ShopServiceTest {
 
     @Test
     void getProduct_shouldReturnProduct_fromProductRepo() {
-        shopService = new ShopService(orderRepo, productRepo);
         assertEquals(product, shopService.getProduct(product.id()));
     }
 
     @Test
     void getAllProducts() {
-        shopService = new ShopService(orderRepo, productRepo);
         List<Product> products = shopService.getAllProducts();
         assertEquals(2, products.size());
-        products.forEach(product -> assertEquals(product, productRepo.getProduct(product.id())));
+        products.forEach(product -> assertEquals(product, productRepo.getProduct(product.id()).orElseThrow()));
     }
 
     @Test
     void addProduct_shouldAddProduct_toProductRepo() {
-        shopService = new ShopService(orderRepo, productRepo);
         assertEquals(2, shopService.getAllProducts().size());
         shopService.addProduct(product);
         assertEquals(2, shopService.getAllProducts().size());
@@ -209,8 +200,6 @@ class ShopServiceTest {
 
     @Test
     void getOrdersWithOrderStatus_shouldReturnOrders_withOrderStatusOrEmptyList() {
-        shopService = new ShopService(orderRepo, productRepo);
-
         assertEquals(0, shopService.getOrdersWithOrderStatus(OrderStatus.PROCESSING).size());
         assertEquals(0, shopService.getOrdersWithOrderStatus(OrderStatus.IN_DELIVERY).size());
         assertEquals(0, shopService.getOrdersWithOrderStatus(OrderStatus.COMPLETED).size());
@@ -223,6 +212,44 @@ class ShopServiceTest {
         assertEquals(2, shopService.getOrdersWithOrderStatus(OrderStatus.PROCESSING).size());
         assertEquals(1, shopService.getOrdersWithOrderStatus(OrderStatus.IN_DELIVERY).size());
         assertEquals(1, shopService.getOrdersWithOrderStatus(OrderStatus.COMPLETED).size());
+    }
+
+    @Test
+    void checkAvailabilityAndStockQuantity_shouldThrowAnException_whenProductDoesNotExist() {
+        UUID id = UUID.randomUUID();
+        ProductWithIdNotFound exception = assertThrows(ProductWithIdNotFound.class, () -> shopService.checkAvailabilityAndStockQuantity(id, 30));
+        assertEquals("Product with id " + id + " not found", exception.getMessage());
+    }
+
+    @Test
+    void checkAvailabilityAndStockQuantity_shouldThrowAnException_whenStockQuantityIsNotEnough() {
+        productRepo.addProduct(product);
+        ProductWithTheIdIsOutOfStock exception = assertThrows(ProductWithTheIdIsOutOfStock.class, () -> shopService.checkAvailabilityAndStockQuantity(product.id(), 50));
+        assertEquals("Product with id " + product.id() + " out of stock", exception.getMessage());
+    }
+
+    @Test
+    void decreaseStockQuantity_shouldThrowAnException_whenProductDoesNotExist() {
+        UUID id = UUID.randomUUID();
+        ProductWithIdNotFound exception = assertThrows(ProductWithIdNotFound.class, () -> shopService.decreaseStockQuantity(id, 30));
+        assertEquals("Product with id " + id + " not found", exception.getMessage());
+    }
+
+    @Test
+    void decreaseStockQuantity_shouldThrowAnException_whenStockQuantityIsNotEnough() {
+        productRepo.addProduct(product);
+        ProductWithTheIdIsOutOfStock exception = assertThrows(ProductWithTheIdIsOutOfStock.class, () -> shopService.checkAvailabilityAndStockQuantity(product.id(), 100));
+        assertEquals("Product with id " + product.id() + " out of stock", exception.getMessage());
+    }
+
+
+    @Test
+    void decreaseStockQuantity_shouldDecreaseStockQuantity_whenStockQuantityIsEnough() {
+        productRepo.addProduct(product);
+        Product decreasedProduct = shopService.decreaseStockQuantity(product.id(), 30);
+        Product updatedProduct = shopService.getProduct(product.id());
+        assertEquals(decreasedProduct, updatedProduct);
+
     }
 
 
